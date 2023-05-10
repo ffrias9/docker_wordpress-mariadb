9af0fd3cc0ac0ad8aa09bc1d88f8ee32ba02e9d6
ffrias9 <framuxs@hotmail.com>
2023-05-10 12:44:35 +0200

    Entrypoint modificado para corregir errores

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-service mariadb start
+sed -i "s/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf;
 
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$ROOT_PASSWD' WITH GRANT OPTION;"
+/etc/init.d/mariadb start
 
-mysql -u root -p $ROOT_PASSWD <<_EOF_
+mysql -u root -p$ROOT_PASSWD <<_EOF_
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 DROP DATABASE IF EXISTS test;
@@ -14,9 +14,5 @@ CREATE DATABASE $NEW_DATABASE;
 GRANT ALL ON $NEW_DATABASE.* TO '$NEW_USER'@'$NEW_USER_HOST';
 FLUSH PRIVILEGES;
 _EOF_
-
-sed -i "s/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf;
-
-service mariadb restart
 
 tail -f /dev/null
