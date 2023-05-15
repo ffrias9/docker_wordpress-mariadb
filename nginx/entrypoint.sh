0f0f2f8a9570dccf0852ab51468e74e887d0947f
ffrias9 <framuxs@hotmail.com>
2023-05-15 11:59:26 +0200

    Modificados Dockerfile y entrypoint para correción de error que impide persistencia

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,21 +2,9 @@
 
 sed -i "s/80$/8081/g" /etc/nginx/sites-available/default
 
-sed -i -e '/server_name _;/a \ \n\tlocation /wordpress {\n\
-	\ \t\tproxy_pass http://'$WORDPRESS_SERVER_IP'/wordpress;\n\
-	\ \t}' /etc/nginx/sites-available/default
+sed -i "s/192.168.10.51:8080/$WORDPRESS_SERVER_IP/g" /etc/nginx/sites-available/default
 
-echo -e "server{\n
-      listen 8081;\n
-      location /wordpress {\n
-              proxy_pass http://$WORDPRESS_SERVER_IP/wordpress;\n
-              proxy_buffering off;\n
-              proxy_set_header X-Real-IP \$remote_addr;\n
-              proxy_set_header X-Forwarded-Host \$host;\n
-              proxy_set_header X-Forwarded-Port \$server_port;\n
-}}" > /etc/nginx/sites-available/wordpress.conf
-
-ln -s /etc/nginx/sites-available/wordpress.conf /etc/nginx/sites-enabled/
+sed -i "s/192.168.10.51:8080/$WORDPRESS_SERVER_IP/g" /etc/nginx/sites-available/wordpress.conf
 
 /etc/init.d/nginx start -g daemon off;
 
