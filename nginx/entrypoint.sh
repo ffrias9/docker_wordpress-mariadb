4717b972e825a919d37dfe59c90ff52e775381e1
ffrias9 <framuxs@hotmail.com>
2023-06-15 13:16:15 +0200

    Now Nginx listen on port 80

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i "s/80$/8081/g" /etc/nginx/sites-available/default
+#sed -i "s/80$/8081/g" /etc/nginx/sites-available/default
 
 sed -i "s/192.168.10.51:8080/$WORDPRESS_SERVER_IP/g" /etc/nginx/sites-available/default
 
