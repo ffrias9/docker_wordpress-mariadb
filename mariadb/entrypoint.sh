7b7340d601943310aabdfe16892e0a9dea26cc35
ffrias9 <framuxs@hotmail.com>
2023-06-22 12:51:52 +0200

    Modified to allow persistence in kubernetes

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -2,9 +2,11 @@
 
 sed -i "s/bind-address            = 127.0.0.1/bind-address            = 0.0.0.0/g" /etc/mysql/mariadb.conf.d/50-server.cnf;
 
+cp -r -n /tmp/* /var/lib/mysql
+
 /etc/init.d/mariadb start
 
-mysql -u root -ptoor <<_EOF_
+mysql -u root -p$ROOT_PASSWD <<_EOF_
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASSWD';
 DELETE FROM mysql.user WHERE User='';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
